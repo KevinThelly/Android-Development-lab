b2dbd07d693c95c4849b89022ad9e71a60d9c37d
KevinThelly <kevin.thelly@gmail.com>
2020-01-14 21:55:52 +0530

    initial commit

---
+++ Signup_form/Signup_Form/app/src/main/java/com/example/ex1/MainActivity.java
@@ -0,0 +1,158 @@
+package com.example.ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.sql.Time;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name;
+    EditText address;
+    EditText email;
+    EditText number;
+    EditText dob;
+    RadioButton rb1;
+    RadioButton rb2;
+    CheckBox ch1;
+    CheckBox ch2;
+    Button b1;
+    Button b2;
+    Button b3;
+    RadioGroup rg;
+//    Spinner spin;
+    Button selectDate;
+    TextView date;
+    DatePickerDialog datePickerDialog;
+    Spinner marriage;
+    TimePicker time;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name=findViewById(R.id.et1);
+        address=findViewById(R.id.et2);
+        email=findViewById(R.id.et4);
+        number=findViewById(R.id.et3);
+        dob=findViewById(R.id.editText2);
+        rb1=findViewById(R.id.radioButton1);
+        rb2=findViewById(R.id.radioButton2);
+        ch1=findViewById(R.id.checkBox);
+        ch2=findViewById(R.id.checkBox1);
+        b1=findViewById(R.id.btnDate);
+        b2=findViewById(R.id.b2);
+        b3=findViewById(R.id.b3);
+        rg=findViewById(R.id.rg);
+        marriage=findViewById(R.id.spin);
+        time=findViewById(R.id.time);
+
+
+        selectDate = findViewById(R.id.btnDate);
+        date = findViewById(R.id.editText2);
+
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                date.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                name.setText("");
+                address.setText("");
+                email.setText("");
+                number.setText("");
+                dob.setText("");
+                rg.clearCheck();
+                if(ch1.isChecked())
+                        ch1.toggle();
+                if(ch2.isChecked())
+                        ch2.toggle();
+
+                }
+            });
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String lan = "";
+                // get selected radio button from radioGroup
+                int selectedId = rg.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                RadioButton radioButton = (RadioButton) findViewById(selectedId);
+
+                Intent i = new Intent(MainActivity.this, tables.class);
+                i.putExtra("name",  name.getText().toString());
+                i.putExtra("addr",address.getText().toString());
+                i.putExtra("email",email.getText().toString());
+                i.putExtra("dob",dob.getText().toString());
+                i.putExtra("contact",number.getText().toString());
+                i.putExtra("gender",radioButton.getText());
+                i.putExtra("marriage",marriage.getSelectedItem().toString());
+                i.putExtra("Hour",time.getCurrentHour().toString());
+                i.putExtra("Min",time.getCurrentMinute().toString());
+                if(ch1.isChecked()){
+                    lan = lan+"smoking";
+                }
+                if (ch1.isChecked() && ch2.isChecked()){
+                    lan = lan+", ";
+                }
+                if(ch2.isChecked()){
+                    lan = lan+"alcohol";
+                }
+                i.putExtra("lang",lan);
+//                Toast.makeText(MainActivity.this, name.getText(), Toast.LENGTH_SHORT).show();
+                startActivity(i);
+            }
+        });
+
+        }
+}
+
+
+
+
+
+
+
+
---
+++ Signup_form/app/src/main/java/com/example/ex1/tables.java
@@ -0,0 +1,36 @@
+package com.example.ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class tables extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tables);
+
+        TextView name, addr, dob, contact, gender, lang, email;
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        addr = findViewById(R.id.addr);
+        dob = findViewById(R.id.date);
+        contact = findViewById(R.id.contact);
+        gender = findViewById(R.id.gender);
+        lang = findViewById(R.id.language);
+
+        Toast.makeText(this, getIntent().getStringExtra("name"), Toast.LENGTH_SHORT).show();
+
+        //getting data from the intent
+        name.setText(getIntent().getStringExtra("name"));
+        email.setText(getIntent().getStringExtra("email"));
+        addr.setText(getIntent().getStringExtra("addr"));
+        dob.setText(getIntent().getStringExtra("dob"));
+        gender.setText(getIntent().getStringExtra("gender"));
+        lang.setText(getIntent().getStringExtra("lang"));
+        contact.setText(getIntent().getStringExtra("contact"));
+    }
+}
